50aab108430215ca8a2876ebd22296621f0a8ef8
CarpeDiem0110 <eEren_Ceylan@hotmail.com>
2023-04-01 02:59:09 +0300

    first commit

---
+++ HomeworksNode/ODEV1/index.js
@@ -0,0 +1,9 @@
+/* Circle area calculator */ 
+let variable = 3 ; 
+
+function calculateArea(perimeter){
+    console.log("Area of your circle is " + (Math.PI*perimeter*perimeter))
+}
+
+
+calculateArea(variable);
